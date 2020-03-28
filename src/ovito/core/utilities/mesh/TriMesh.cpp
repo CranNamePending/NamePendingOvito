f05925d42b92faab65a2dcb0bf7dda279511a062
Alexander Stukowski <stukowski@mm.tu-darmstadt.de>
2020-03-28 22:39:46 +0100

    Fixed compilation error in TriMesh.cpp under Windows

--- src/ovito/core/utilities/mesh/TriMesh.cpp
+++ src/ovito/core/utilities/mesh/TriMesh.cpp
@@ -552,13 +552,13 @@ TriMesh TriMesh::createIcosphere(int resolution)
 	constexpr FloatType Z = 0.850650808352039932;
 	constexpr FloatType N = 0.0;
 
-	static constexpr Point3 vertices[] = {
+	static const Point3 vertices[] = {
 		{-X,N,Z}, {X,N,Z}, {-X,N,-Z}, {X,N,-Z},
 		{N,Z,X}, {N,Z,-X}, {N,-Z,X}, {N,-Z,-X},
 		{Z,X,N}, {-Z,X, N}, {Z,-X,N}, {-Z,-X, N}
 	};
 
-	static constexpr std::array<int,3> triangles[] = {
+	static const std::array<int,3> triangles[] = {
 		{0,4,1}, {0,9,4}, {9,5,4}, {4,5,8}, {4,8,1},
 		{8,10,1}, {8,3,10}, {5,3,8}, {5,2,3}, {2,7,3},
 		{7,10,3}, {7,6,10}, {7,11,6}, {11,0,6}, {0,1,6},
